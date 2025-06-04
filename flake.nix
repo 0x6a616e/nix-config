e4a5254d03cf7924d74a5a97cd868e78a96254eb
jan <jan.reyes.contact@gmail.com>
2025-06-03 22:16:50 -0600

    feat: added basic flake

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations = {
+      failsafe = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./configuration.nix
+        ];
+      };
+    };
+  };
+}
