6c6f351def73fb04327991ba98c3e63af116a6aa
jan <jan.reyes.contact@gmail.com>
2025-06-03 22:57:13 -0600

    feat: added git

--- hosts/failsafe/home.nix
+++ hosts/failsafe/home.nix
@@ -7,6 +7,12 @@
   home.username = "jan";
   home.homeDirectory = "/home/jan";
 
+  programs.git = {
+    enable = true;
+    userEmail = "jan.reyes.contact@gmail.com";
+    userName = "jan";
+  };
+
   home.stateVersion = "24.05";
 
   programs.home-manager.enable = true;
