f52b83fb17208d7c6b2641365b4ef88ae23abb2c
jan <jan.reyes.contact@gmail.com>
2025-06-17 23:00:31 -0600

    fix: fixing catppuccin

--- hosts/failsafe/home.nix
+++ hosts/failsafe/home.nix
@@ -1,11 +1,15 @@
-{ ... }:
+{ inputs, ... }:
 {
   imports = [
     ./fonts.nix
     ./git.nix
     ./hyprland.nix
     ./kitty.nix
+
+    inputs.catppuccin.homeModules.catppuccin
   ];
+
+  catppuccin.kitty.enable = true;
 
   home = {
     username = "jan";
