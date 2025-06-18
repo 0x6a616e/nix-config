8eb097a2aad4942e19fe51734bf243417a27eba8
jan <jan.reyes.contact@gmail.com>
2025-06-17 20:49:31 -0600

    feat: extracted all functions

--- hosts/failsafe/home.nix
+++ hosts/failsafe/home.nix
@@ -3,28 +3,14 @@
   imports = [
     ./fonts.nix
     ./git.nix
+    ./hyprland.nix
+    ./kitty.nix
   ];
 
   home = {
     username = "jan";
     homeDirectory = "/home/jan";
     stateVersion = "24.05";
-  };
-
-  programs.kitty = {
-    enable = true;
-    font.name = "JetBrains Mono Nerd Font";
-  };
-
-  wayland.windowManager.hyprland = {
-    enable = true;
-    settings = {
-      "$mod" = "SUPER";
-      "$terminal" = "kitty";
-      bind = [
-        "$mod, Q, exec, $terminal"
-      ];
-    };
   };
 
   programs.home-manager.enable = true;
