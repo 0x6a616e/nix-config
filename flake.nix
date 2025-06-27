55fab67110ab384fdd059be43bda10ae4deb960f
jan <jan.reyes.contact@gmail.com>
2025-06-26 23:26:16 -0600

    feat: properly divided config

--- flake.nix
+++ flake.nix
@@ -3,13 +3,24 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/home-manager";
+    };
   };
 
-  outputs = { self, nixpkgs, ... } @ inputs: {
+  outputs = { self, nixpkgs, home-manager, ... } @ inputs: {
     nixosConfigurations = {
       failsafe = nixpkgs.lib.nixosSystem {
         modules = [ ./hosts/failsafe ];
         specialArgs = { inherit inputs; };
+      };
+    };
+
+    homeConfigurations = {
+      "jan@failsafe" = home-manager.lib.homeManagerConfiguration {
+        modules = [ ];
+        speacialArgs = { };
       };
     };
   };
