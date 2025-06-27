c336a992c310cc0968edff28f54bf6f81ee85d0f
jan <jan.reyes.contact@gmail.com>
2025-06-26 22:38:46 -0600

    Revert "feat: testing catppuccin theme for kitty"
    This reverts commit 5eed6395776e45951f126b7bb5192028c3829a3e.

--- hosts/failsafe/kitty.nix
+++ hosts/failsafe/kitty.nix
@@ -1,11 +1,5 @@
-{ inputs, ... }:
+{ ... }:
 {
-  import = [
-    inputs.catppuccin.homeModules.catppuccin
-  ];
-
-  catppuccin.kitty.enable = true;
-
   programs.kitty = {
     enable = true;
     font.name = "JetBrains Mono Nerd Font";
