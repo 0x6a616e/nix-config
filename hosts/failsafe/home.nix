0ff5a50c0a53900ae7e7aa62d6fe5e27b716ff5f
jan <jan.reyes.contact@gmail.com>
2025-06-26 22:38:33 -0600

    Revert "feat: going monolith for now"
    This reverts commit ff00dd68a92699f2cf3c6b595ba235caab51d6fc.

--- hosts/failsafe/home.nix
+++ hosts/failsafe/home.nix
@@ -1,58 +1,12 @@
-{ inputs, pkgs, ... }:
+{ inputs, ... }:
 {
-  import = [
-    inputs.catppuccin.homeModules.catppuccin
+  imports = [
+    ./fonts.nix
+    ./git.nix
+    ./hyprland.nix
+    (import ./kitty.nix { inherit inputs; })
   ];
 
-  # --- Fonts ---
-  fonts = {
-    fontconfig.enable = true;
-  };
-
-  home.packages = [
-    pkgs.nerd-fonts.jetbrains-mono
-  ];
-
-  # --- Git ---
-  programs.git = {
-    enable = true;
-    userEmail = "jan.reyes.contact@gmail.com";
-    userName = "jan";
-  };
-
-  # --- Hyprland
-  wayland.windowManager.hyprland = {
-    enable = true;
-    settings = {
-      "$mod" = "SUPER";
-      "$terminal" = "kitty";
-      bind = [
-        "$mod, Q, exec, $terminal"
-      ];
-    };
-  };
-
-  # --- Kitty ---
-  catppuccin.kitty = {
-    enable = true;
-    flavor = "mocha";
-  };
-
-  programs.kitty = {
-    enable = true;
-    font = {
-      name = "JetBrains Mono Nerd Font";
-      size = 11.25;
-    };
-    settings = {
-      window_padding_width = 5;
-      tab_bar_min_tabs = 1;
-      tab_bar_edge = "bottom";
-      tab_bar_style = "powerline";
-      tab_powerline_style = "slanted";
-    };
-  };
-  
   home = {
     username = "jan";
     homeDirectory = "/home/jan";
---
+++ hosts/failsafe/hyprland.nix
@@ -0,0 +1,13 @@
+{ ... }:
+{
+  wayland.windowManager.hyprland = {
+    enable = true;
+    settings = {
+      "$mod" = "SUPER";
+      "$terminal" = "kitty";
+      bind = [
+        "$mod, Q, exec, $terminal"
+      ];
+    };
+  };
+}
