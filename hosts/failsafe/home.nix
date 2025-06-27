9faee840b3ad5730ed2fcf395302ec47fc57039b
jan <jan.reyes.contact@gmail.com>
2025-06-26 22:39:13 -0600

    Revert "feat: added git"
    This reverts commit 6c6f351def73fb04327991ba98c3e63af116a6aa.

--- hosts/failsafe/home.nix
+++ hosts/failsafe/home.nix
@@ -7,12 +7,6 @@
   home.username = "jan";
   home.homeDirectory = "/home/jan";
 
-  programs.git = {
-    enable = true;
-    userEmail = "jan.reyes.contact@gmail.com";
-    userName = "jan";
-  };
-
   home.stateVersion = "24.05";
 
   programs.home-manager.enable = true;
