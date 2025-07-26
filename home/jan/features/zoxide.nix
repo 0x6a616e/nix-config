1dd400d427384259f5c81e20364b3ecfb8ff3c25
jan <jan.reyes.contact@gmail.com>
2025-07-26 16:56:04 -0600

    fixed misstype

--- home/jan/features/zoxide.nix
+++ home/jan/features/zoxide.nix
@@ -3,6 +3,8 @@
     programs.zoxide = {
         enable = true;
         enableZshIntegration = true;
-        options = "--cmd cd";
+        options = [
+            "--cmd cd"
+        ];
     };
 }
