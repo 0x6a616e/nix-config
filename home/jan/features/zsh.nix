6f6b430d44439d800d9b618481b426a53504f052
jan <jan.reyes.contact@gmail.com>
2025-07-26 14:11:19 -0600

    more zsh configs

--- home/jan/features/zsh.nix
+++ home/jan/features/zsh.nix
@@ -1,7 +1,16 @@
 { ... }:
 {
     programs.zsh = {
+        autosuggestion.enable = true;
         enable = true;
+        history = {
+            append = true;
+            ignoreAllDups = true;
+            ignoreSpace = true;
+            save = 10000;
+            share = true;
+            size = 10000;
+        };
         initContent = ''
             PS1="%B%T%b %F{cyan}%0~%f$NEWLINE%F{cyan}~>%f ";
         '';
