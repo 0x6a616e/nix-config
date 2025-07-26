32e14b3dcf1aaba140a94bb2b029a8e09d065c2b
jan <jan.reyes.contact@gmail.com>
2025-07-26 13:02:46 -0600

    fixing special char

--- home/jan/features/zsh.nix
+++ home/jan/features/zsh.nix
@@ -3,7 +3,7 @@
     programs.zsh = {
         enable = true;
         sessionVariables = {
-            NEWLINE = "\n";
+            NEWLINE = "\\n";
             PS1 = "%B%T%b %F{cyan}%0~%f$NEWLINE%F{cyan}~>%f";
         };
         setOptions = [
