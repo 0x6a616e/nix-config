a42f0d795c3db2195bdaa26ce19f892834486b36
jan <jan.reyes.contact@gmail.com>
2025-07-26 17:57:44 -0600

    I may be a dumbass

--- home/jan/features/kitty.nix
+++ home/jan/features/kitty.nix
@@ -3,14 +3,13 @@
     programs.kitty = {
         enable = true;
         font = {
-            name = "JetBrains Mono";
+            name = "JetBrainsMono Nerd Font";
             size = 11.25;
         };
         keybindings = {
             "ctrl+shift+t" = "new_tab_with_cwd";
         };
         settings = {
-            font-features = "+liga";
             tab_bar_min_tabs = 1;
             tab_bar_style = "powerline";
             tab_powerline_style = "slanted";
