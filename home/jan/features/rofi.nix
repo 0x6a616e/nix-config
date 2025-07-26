07b712007bd60539392aae8c439381f8dff0f314
jan <jan.reyes.contact@gmail.com>
2025-07-26 17:40:10 -0600

    all rofi conf

--- home/jan/features/rofi.nix
+++ home/jan/features/rofi.nix
@@ -8,12 +8,19 @@
     programs.rofi = {
         enable = true;
         extraConfig = {
+            disable-history = false;
+            display-drun = "   Apps ";
+            display-network = " 󰤨  Network";
+            display-run = "   Run ";
+            display-window = " 󰕰  Window";
+            drun-display-format = "{icon} {name}";
+            hide-scrollbar = true;
             icon-theme = "Oranchelo";
             show-icons = true;
+            sidebar-mode = true;
         };
         modes = [
             "drun"
         ];
-        terminal = "kitty";
     };
 }
