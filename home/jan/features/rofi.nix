ba171bcf365eddb8aef18372ed09f6b984075b41
jan <jan.reyes.contact@gmail.com>
2025-07-26 18:06:23 -0600

    about to test rofi options

--- home/jan/features/rofi.nix
+++ home/jan/features/rofi.nix
@@ -1,24 +1,25 @@
 { ... }:
 {
     catppuccin.rofi = {
-        # enable = true;
+        enable = true;
         flavor = "mocha";
     };
 
     programs.rofi = {
         enable = true;
         extraConfig = {
-            disable-history = false;
-            display-drun = "   Apps ";
-            display-network = " 󰤨  Network";
-            display-run = "   Run ";
-            display-window = " 󰕰  Window";
-            drun-display-format = "{icon} {name}";
-            hide-scrollbar = true;
-            icon-theme = "Oranchelo";
-            show-icons = true;
-            sidebar-mode = true;
+            # disable-history = false;
+            # display-drun = "   Apps ";
+            # display-network = " 󰤨  Network";
+            # display-run = "   Run ";
+            # display-window = " 󰕰  Window";
+            # drun-display-format = "{icon} {name}";
+            # hide-scrollbar = true;
+            # icon-theme = "Oranchelo";
+            # show-icons = true;
+            # sidebar-mode = true;
         };
+        font = "JetBrainsMono Nerd Font";
         modes = [
             "drun"
         ];
