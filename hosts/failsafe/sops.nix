242a5345286b23783325c51cb5da43706eb010cd
jan <jan.reyes.contact@gmail.com>
2025-11-16 21:08:08 -0600

    implemented auto tailscale

--- hosts/failsafe/sops.nix
+++ hosts/failsafe/sops.nix
@@ -1,6 +1,6 @@
-{ ... }:
+{ config, ... }:
 {
     sops.defaultSopsFile = ../../secrets/secrets.yaml;
-    sops.age.keyFile = "$HOME/.config/sops/age/keys.txt";
+    sops.age.keyFile = "${config.users.users.jan.home}/.config/sops/age/keys.txt";
     sops.secrets."tailscale/authKey" = { };
 }
