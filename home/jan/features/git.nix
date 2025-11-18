aa8a81b8859168021cb46fe4c35101d3b3c6d6ac
jan <jan.reyes.contact@gmail.com>
2025-11-17 19:44:01 -0600

    fixed git

--- home/jan/features/git.nix
+++ home/jan/features/git.nix
@@ -2,7 +2,9 @@
 {
     programs.git = {
         enable = true;
-        userName = "jan";
-        userEmail = "jan.reyes.contact@gmail.com";
+        user = {
+            name = "jan";
+            email = "jan.reyes.contact@gmail.com";
+        };
     };
 }
