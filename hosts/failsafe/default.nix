272af1d509236ad0968547bdb39b7d7a3900fca0
jan <jan.reyes.contact@gmail.com>
2025-11-17 19:19:07 -0600

    removed nix gc

--- hosts/failsafe/default.nix
+++ hosts/failsafe/default.nix
@@ -45,11 +45,6 @@
     };
 
     nix = {
-        gc = {
-            automatic = true;
-            dates = "weekly";
-            options = "--delete-older-than 7d";
-        };
         settings = {
             auto-optimise-store = true;
             experimental-features = [ "nix-command" "flakes" ];
