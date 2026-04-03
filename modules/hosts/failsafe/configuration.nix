111a85ad0f55e4ea8771247dc7dbf264ec04d591
jan <jan.reyes.contact@gmail.com>
2026-04-03 11:36:30 -0600

    new installation

--- modules/hosts/failsafe/configuration.nix
+++ modules/hosts/failsafe/configuration.nix
@@ -21,7 +21,20 @@
             enable32Bit = true;
         };
 
-        i18n.defaultLocale = "en_US.UTF-8";
+	i18n = {
+		defaultLocale = "en_US.UTF-8";
+		extraLocaleSettings = {
+			LC_ADDRESS = "es_MX.UTF-8";
+			LC_IDENTIFICATION = "es_MX.UTF-8";
+			LC_MEASUREMENT = "es_MX.UTF-8";
+			LC_MONETARY = "es_MX.UTF-8";
+			LC_NAME = "es_MX.UTF-8";
+			LC_NUMERIC = "es_MX.UTF-8";
+			LC_PAPER = "es_MX.UTF-8";
+			LC_TELEPHONE = "es_MX.UTF-8";
+			LC_TIME = "es_MX.UTF-8";
+		};
+	};
 
         networking = {
             hostName = "failsafe";
@@ -45,9 +58,9 @@
         time.timeZone = "America/Monterrey";
 
         users.users.jan = {
+            isNormalUser = true;
             description = "Jan";
             extraGroups = [ "networkmanager" "wheel" ];
-            isNormalUser = true;
         };
     };
 }
--- modules/hosts/failsafe/hardware.nix
+++ modules/hosts/failsafe/hardware.nix
@@ -12,21 +12,21 @@
         };
 
         fileSystems."/" = {
-            device = "/dev/disk/by-uuid/3cc69dba-66c4-443b-81f4-1dcce3c37e48";
+            device = "/dev/disk/by-uuid/c6de48b1-cf23-44e9-a393-235d38c01eaf";
             fsType = "ext4";
         };
 
         fileSystems."/boot" = {
-            device = "/dev/disk/by-uuid/1F13-35BB";
+            device = "/dev/disk/by-uuid/3B77-F358";
             fsType = "vfat";
             options = [ "fmask=0077" "dmask=0077" ];
         };
 
-        swapDevices = [ { device = "/dev/disk/by-uuid/492e8c67-b662-4a3e-933e-1a1e5d1bf05a"; } ];
+        swapDevices = [ ];
 
         networking.useDHCP = lib.mkDefault true;
 
-        nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+	nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
         hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
     };
 }
