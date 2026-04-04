30bc2004189af41408770c27eca3e7254eaf0aa9
jan <jan.reyes.contact@gmail.com>
2026-04-04 03:40:08 -0600

    removed old files

--- legacy/hosts/features/hyprland.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, ... }:
-{
-    environment.systemPackages = [
-        pkgs.wl-clipboard
-    ];
-    programs.hyprland.enable = true;
-    security.pam.services.hyprlock = {};
-}
--- legacy/hosts/features/sops.nix
+++
@@ -1,6 +0,0 @@
-{ config, ... }:
-{
-    sops.defaultSopsFile = ../../secrets/secrets.yaml;
-    sops.age.keyFile = "${config.users.users.jan.home}/.config/sops/age/keys.txt";
-    sops.secrets."tailscale/authKey" = { };
-}
