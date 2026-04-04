c7cdb32b0979480be22bde918b518fcd69e37471
jan <jan.reyes.contact@gmail.com>
2026-04-04 00:32:42 -0600

    added rofi

---
+++ modules/features/rofi.nix
@@ -0,0 +1,19 @@
+{ self, inputs, ... }: {
+	flake.homeModules.rofi = { pkgs, ... }: {
+	    programs.rofi = {
+		enable = true;
+		extraConfig = {
+		    disable-history = false;
+		    display-drun = "   Apps ";
+		    display-network = " 󰤨  Network";
+		    display-run = "   Run ";
+		    display-window = " 󰕰  Window";
+		    drun-display-format = "{icon} {name}";
+		    icon-theme = "Oranchelo";
+		    show-icons = true;
+		};
+		font = "JetBrainsMono Nerd Font 12";
+		modes = [ "drun" ];
+	    };
+	};
+}
