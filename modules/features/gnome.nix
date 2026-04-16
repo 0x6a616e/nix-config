4b3d8bd14da1dec85c058b9a6d02b7de0fc3d782
jan <jan.reyes.contact@gmail.com>
2026-04-16 02:39:51 -0600

    added some more dconfs

--- modules/features/gnome.nix
+++ modules/features/gnome.nix
@@ -4,16 +4,11 @@
             self.nixosModules.fonts
         ];
 
-		# environment.systemPackages = [
-		#           pkgs.nautilus
-		# 	pkgs.wl-clipboard
-		# ];
-
         services = {
             displayManager.gdm.enable = true;
             desktopManager.gnome.enable = true;
             gnome = {
-                core-apps.enable = false; # activate passwords-and-keys
+                core-apps.enable = false;
                 core-developer-tools.enable = false;
                 games.enable = false;
             };
@@ -21,7 +16,10 @@
         environment.gnome.excludePackages = with pkgs; [ gnome-tour gnome-user-docs ];
     };
 
-    flake.homeModules.gnome = { pkgs, lib, ... }: {
+    flake.homeModules.gnome = { pkgs, lib, ... }:
+    let
+        wallpaper = ../../assets/wallpaper.jpg;
+    in {
         imports = [
             self.homeModules.kitty
             self.homeModules.firefox
@@ -64,6 +62,17 @@
         dconf = {
             enable = true;
             settings = {
+                "org/gnome/desktop/app-folders" = {
+                    folder-children = [ ];
+                };
+                "org/gnome/desktop/background" = {
+                    color-shading-type = "solid";
+                    picture-options = "zoom";
+                    picture-uri = "file://${wallpaper}";
+                    picture-uri-dark = "file://${wallpaper}";
+                    primary-color = "#000000";
+                    secondary-color = "#000000";
+                };
                 "org/gnome/desktop/input-sources" = {
                     sources = [ (lib.hm.gvariant.mkTuple [ "xkb" "us" ]) ];
                     xkb-options = [ "compose:ralt" ];
@@ -72,6 +81,19 @@
                     accent-color = "yellow";
                     color-scheme = "prefer-dark";
                     clock-format = "24h";
+                };
+                "org/gnome/desktop/notifications" = {
+                    application-children = [ "org-gnome-nautilus" ];
+                };
+                "org/gnome/desktop/notifications/application/org-gnome-nautilus" = {
+                    application-id = "org.gnome.Nautilus.desktop";
+                };
+                "org/gnome/desktop/screensaver" = {
+                    color-shading-type = "solid";
+                    picture-options = "zoom";
+                    picture-uri = "file://${wallpaper}";
+                    primary-color = "#000000";
+                    secondary-color = "#000000";
                 };
                 "org/gnome/desktop/screen-time-limits" = {
                     daily-limit-enabled = false;
@@ -86,151 +108,11 @@
                 "org/gnome/settings-daemon/plugins/power" = {
                     sleep-inactive-ac-timeout = 900;
                 };
-                "org/gnome/desktop/notifications" = {
-                    application-children = [ "org-gnome-nautilus" ];
-                };
-                "org/gnome/desktop/notifications/application/org-gnome-nautilus" = {
-                    application-id = "org.gnome.Nautilus.desktop";
-                };
                 "org/gnome/shell" = {
                     enabled-extensions = [ "caffeine@patapon.info" ];
                     favorite-apps = [];
                 };
-                # wallpaper
             };
         };
     };
 }
-# # Generated via dconf2nix: https://github.com/gvolpe/dconf2nix
-# { lib, ... }:
-#
-# with lib.hm.gvariant;
-#
-# {
-#   dconf.settings = {
-#     "org/gnome/control-center" = {
-#       last-panel = "wifi";
-#       window-state = mkTuple [ 980 640 true ];
-#     };
-#
-#     "org/gnome/desktop/app-folders" = {
-#       folder-children = [ "System" "Utilities" "YaST" "Pardus" ];
-#     };
-#
-#     "org/gnome/desktop/app-folders/folders/Pardus" = {
-#       categories = [ "X-Pardus-Apps" ];
-#       name = "X-Pardus-Apps.directory";
-#       translate = true;
-#     };
-#
-#     "org/gnome/desktop/app-folders/folders/System" = {
-#       apps = [ "org.gnome.DiskUtility.desktop" "org.gnome.Logs.desktop" ];
-#       name = "X-GNOME-Shell-System.directory";
-#       translate = true;
-#     };
-#
-#     "org/gnome/desktop/app-folders/folders/Utilities" = {
-#       apps = [ "org.gnome.Loupe.desktop" ];
-#       name = "X-GNOME-Shell-Utilities.directory";
-#       translate = true;
-#     };
-#
-#     "org/gnome/desktop/app-folders/folders/YaST" = {
-#       categories = [ "X-SuSE-YaST" ];
-#       name = "suse-yast.directory";
-#       translate = true;
-#     };
-#
-#     "org/gnome/desktop/background" = {
-#       color-shading-type = "solid";
-#       picture-options = "zoom";
-#       picture-uri = "file:///home/jan/.local/share/backgrounds/2026-04-15-23-10-30-wallpaper.jpg";
-#       picture-uri-dark = "file:///home/jan/.local/share/backgrounds/2026-04-15-23-10-30-wallpaper.jpg";
-#       primary-color = "#000000000000";
-#       secondary-color = "#000000000000";
-#     };
-#
-#     "org/gnome/desktop/input-sources" = {
-#       sources = [ (mkTuple [ "xkb" "us" ]) ];
-#       xkb-options = [ "compose:ralt" ];
-#     };
-#
-#     "org/gnome/desktop/interface" = {
-#       accent-color = "yellow";
-#       clock-format = "24h";
-#       color-scheme = "prefer-dark";
-#     };
-#
-#     "org/gnome/desktop/notifications" = {
-#       application-children = [ "org-gnome-nautilus" ];
-#     };
-#
-#     "org/gnome/desktop/notifications/application/org-gnome-nautilus" = {
-#       application-id = "org.gnome.Nautilus.desktop";
-#     };
-#
-#     "org/gnome/desktop/peripherals/touchpad" = {
-#       two-finger-scrolling-enabled = true;
-#     };
-#
-#     "org/gnome/desktop/screen-time-limits" = {
-#       daily-limit-enabled = false;
-#       history-enabled = false;
-#     };
-#
-#     "org/gnome/desktop/screensaver" = {
-#       color-shading-type = "solid";
-#       picture-options = "zoom";
-#       picture-uri = "file:///home/jan/.local/share/backgrounds/2026-04-15-23-10-30-wallpaper.jpg";
-#       primary-color = "#000000000000";
-#       secondary-color = "#000000000000";
-#     };
-#
-#     "org/gnome/desktop/search-providers" = {
-#       disable-external = true;
-#     };
-#
-#     "org/gnome/desktop/session" = {
-#       idle-delay = mkUint32 300;
-#     };
-#
-#     "org/gnome/evolution-data-server" = {
-#       migrated = true;
-#     };
-#
-#     "org/gnome/nautilus/preferences" = {
-#       default-folder-viewer = "icon-view";
-#       migrated-gtk-settings = true;
-#     };
-#
-#     "org/gnome/nautilus/window-state" = {
-#       initial-size-file-chooser = mkTuple [ 890 550 ];
-#     };
-#
-#     "org/gnome/portal/filechooser/org/gnome/Settings" = {
-#       last-folder-path = "/home/jan/nix-config/assets";
-#     };
-#
-#     "org/gnome/settings-daemon/plugins/color" = {
-#       night-light-schedule-automatic = false;
-#     };
-#
-#     "org/gnome/settings-daemon/plugins/power" = {
-#       sleep-inactive-ac-timeout = 900;
-#     };
-#
-#     "org/gnome/shell" = {
-#       command-history = [ "ls" ];
-#       enabled-extensions = [ "caffeine@patapon.info" ];
-#       favorite-apps = [];
-#       last-selected-power-profile = "performance";
-#       welcome-dialog-last-shown-version = "49.4";
-#     };
-#
-#     "org/gnome/shell/extensions/caffeine" = {
-#       cli-toggle = false;
-#       indicator-position-max = 1;
-#     };
-#
-#   };
-# }
