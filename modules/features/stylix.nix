56600d7a76a849286b952849031b0f12337886e0
jan <jan.reyes.contact@gmail.com>
2026-04-16 21:26:29 -0600

    tmp

--- modules/features/stylix.nix
+++ modules/features/stylix.nix
@@ -1,11 +1,15 @@
 { self, inputs, ... }: {
     flake.homeModules.stylix = { pkgs, ... }: {
-        stylix = {
-            enable = true;
-            base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
-            # base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine.yaml";
-            # base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine-moon.yaml";
-            image = ../../assets/wallpaper.jpg;
-        };
+        imports = [
+            inputs.stylix.homeModules.stylix
+        ];
+
+        # stylix = {
+        #     enable = true;
+        #     # base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+        #     # base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine.yaml";
+        #     # base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine-moon.yaml";
+        #     # image = ../../assets/wallpaper.jpg;
+        # };
     };
 }
