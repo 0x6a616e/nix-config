f3d07e1ea73b08196880d58ed881ef807ab18fdc
jan <jan.reyes.contact@gmail.com>
2026-04-18 19:32:07 -0600

    added theming to firefox

--- modules/features/firefox.nix
+++ modules/features/firefox.nix
@@ -1,61 +1,68 @@
 { self, inputs, ... }: {
 	flake.homeModules.firefox = { pkgs, ... }: {
+                stylix.targets.firefox = {
+                        profileNames = [ "main" ];
+                        colorTheme.enable = true;
+                        # colors.enable = true;
+                };
+
 		programs.firefox = {
-            enable = true;
-            profiles.main = {
-                search = {
-                    force = true;
-                    engines = {
-                        nixos-wiki = {
-                            name = "NixOS Wiki";
-                            urls = [{
-                                template = "https://wiki.nixos.org/w/index.php";
-                                params = [
-                                    { name = "search"; value = "{searchTerms}"; }
-                                ];
-                            }];
-                            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-                            definedAliases = [ "@nw" ];
-                        };
-                        nix-packages = {
-                            name = "Nix Packages";
-                            urls = [{
-                                template = "https://search.nixos.org/packages";
-                                params = [
-                                    { name = "channel"; value = "unstable"; }
-                                    { name = "query"; value = "{searchTerms}"; }
-                                ];
-                            }];
-                            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-                            definedAliases = [ "@np" ];
-                        };
-                        nix-options = {
-                            name = "Nix Options";
-                            urls = [{
-                                template = "https://search.nixos.org/options";
-                                params = [
-                                    { name = "channel"; value = "unstable"; }
-                                    { name = "query"; value = "{searchTerms}"; }
-                                ];
-                            }];
-                            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-                            definedAliases = [ "@no" ];
-                        };
-                        home-options = {
-                            name = "Home Manager Options";
-                            urls = [{
-                                template = "https://home-manager-options.extranix.com/";
-                                params = [
-                                    { name = "release"; value = "master"; }
-                                    { name = "query"; value = "{searchTerms}"; }
-                                ];
-                            }];
-                            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-                            definedAliases = [ "@ho" ];
+                    enable = true;
+                    profiles.main = {
+                        extensions.force = true;
+                        search = {
+                            force = true;
+                            engines = {
+                                nixos-wiki = {
+                                    name = "NixOS Wiki";
+                                    urls = [{
+                                        template = "https://wiki.nixos.org/w/index.php";
+                                        params = [
+                                            { name = "search"; value = "{searchTerms}"; }
+                                        ];
+                                    }];
+                                    icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+                                    definedAliases = [ "@nw" ];
+                                };
+                                nix-packages = {
+                                    name = "Nix Packages";
+                                    urls = [{
+                                        template = "https://search.nixos.org/packages";
+                                        params = [
+                                            { name = "channel"; value = "unstable"; }
+                                            { name = "query"; value = "{searchTerms}"; }
+                                        ];
+                                    }];
+                                    icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+                                    definedAliases = [ "@np" ];
+                                };
+                                nix-options = {
+                                    name = "Nix Options";
+                                    urls = [{
+                                        template = "https://search.nixos.org/options";
+                                        params = [
+                                            { name = "channel"; value = "unstable"; }
+                                            { name = "query"; value = "{searchTerms}"; }
+                                        ];
+                                    }];
+                                    icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+                                    definedAliases = [ "@no" ];
+                                };
+                                home-options = {
+                                    name = "Home Manager Options";
+                                    urls = [{
+                                        template = "https://home-manager-options.extranix.com/";
+                                        params = [
+                                            { name = "release"; value = "master"; }
+                                            { name = "query"; value = "{searchTerms}"; }
+                                        ];
+                                    }];
+                                    icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+                                    definedAliases = [ "@ho" ];
+                                };
+                            };
                         };
                     };
                 };
-            };
-        };
 	};
 }
