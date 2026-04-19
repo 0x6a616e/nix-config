e76e85ba0331c258a577e7d70b975c8482aae093
jan <jan.reyes.contact@gmail.com>
2026-04-18 23:33:50 -0600

    merged configuration and hardware files

--- modules/hosts/failsafe.nix
+++ modules/hosts/failsafe.nix
@@ -1,8 +1,7 @@
 { self, inputs, ... }: {
-    flake.nixosModules.failsafeConfiguration = { pkgs, config, lib, ... }: {
+    flake.nixosModules.failsafeConfiguration = { pkgs, config, lib, modulesPath, ... }: {
         imports = [
-            self.nixosModules.failsafeHardware
-
+            (modulesPath + "/installer/scan/not-detected.nix")
             inputs.home-manager.nixosModules.home-manager
 
             self.nixosModules.nh
@@ -13,10 +12,59 @@
             self.nixosModules.gnome
         ];
 
-        home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            users.jan.imports = [ self.homeModules.jan ];
+        boot = {
+            initrd = {
+                availableKernelModules = [ "nvme" "xhci_pci" "usbhid" "usb_storage" "sd_mod" ];
+                kernelModules = [ ];
+                systemd.services.rollback-root = {
+                    description = "Rollback ZFS root dataset";
+                    wantedBy = [ "initrd.target" ];
+                    after = [ "zfs-import.target" ];
+                    before = [ "sysroot.mount" ];
+
+                    serviceConfig = {
+                        Type = "oneshot";
+                        ExecStart = "${pkgs.zfs}/bin/zfs rollback -r rpool/root@blank";
+                    };
+                };
+            };
+            kernelModules = [ "kvm-amd" ];
+            extraModulePackages = [ ];
+            loader = {
+                efi.canTouchEfiVariables = true;
+                systemd-boot.enable = true;
+            };
+            supportedFilesystems = [ "ntfs" ];
+        };
+
+        fileSystems = {
+            "/" = {
+                device = "rpool/root";
+                fsType = "zfs";
+            };
+            "/nix" = {
+                device = "rpool/nix";
+                fsType = "zfs";
+            };
+            "/persistent" = {
+                device = "rpool/persistent";
+                neededForBoot = true;
+                fsType = "zfs";
+            };
+            "/boot" = {
+                device = "/dev/disk/by-uuid/9D53-5CC6";
+                fsType = "vfat";
+                options = [ "fmask=0077" "dmask=0077" ];
+            };
+        };
+        swapDevices = [ { device = "/dev/disk/by-uuid/3e7c6bc9-200b-48da-b250-2497017930f9"; } ];
+
+        hardware = {
+            cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+            graphics = {
+                enable = true;
+                enable32Bit = true;
+            };
         };
 
         i18n = {
@@ -77,6 +125,12 @@
         system.stateVersion = "25.05";
 
         time.timeZone = "America/Monterrey";
+
+        home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.jan.imports = [ self.homeModules.jan ];
+        };
 
         sops.secrets."users/jan/password" = { };
         sops.secrets."users/jan/password".neededForUsers = true;
