670ef7b149e3d7035c9a14fb306f75d04782fee7
jan <jan.reyes.contact@gmail.com>
2026-04-18 21:54:41 -0600

    updated to nvf

--- modules/features/nvim.nix
+++ modules/features/nvim.nix
@@ -1,268 +1,248 @@
-{
-  self,
-  inputs,
-  ...
-}: {
-  flake.homeModules.nvim = {pkgs, ...}: {
-    imports = [
-      inputs.nixvim.homeModules.nixvim
-      self.homeModules.ripgrep
-      self.homeModules.fd
-    ];
+{ self, inputs, ... }: {
+    flake.homeModules.nvim = { pkgs, lib, ... }: {
+        imports = [
+            inputs.nvf.homeManagerModules.default
+            self.homeModules.ripgrep
+            self.homeModules.fd
+        ];
 
-    stylix.targets.nixvim.enable = false;
-
-    programs.nixvim = {
-      enable = true;
-      autoCmd = [
-        {
-          event = ["TextYankPost"];
-          callback.__raw = ''
-                          				function()
-               		vim.highlight.on_yank()
-            end
-          '';
-          pattern = ["*"];
-        }
-      ];
-      globals = {
-        mapleader = " ";
-        maplocalleader = " ";
-      };
-      opts = {
-        breakindent = true;
-        completeopt = "menuone,noselect";
-        conceallevel = 2;
-        expandtab = true;
-        formatoptions = "ro/";
-        hlsearch = false;
-        ignorecase = true;
-        incsearch = true;
-        number = true;
-        relativenumber = true;
-        scrolloff = 8;
-        shiftwidth = 4;
-        signcolumn = "yes";
-        smartcase = true;
-        smartindent = true;
-        softtabstop = 4;
-        tabstop = 4;
-        termguicolors = true;
-        undofile = true;
-        wrap = false;
-      };
-      keymaps = [
-        {
-          mode = ["n" "v"];
-          key = "<Space>";
-          action = "<Nop>";
-          options.silent = true;
-        }
-        {
-          mode = "v";
-          key = "J";
-          action = ":m '>+1<CR>gv=gv";
-        }
-        {
-          mode = "v";
-          key = "K";
-          action = ":m '<-2<CR>gv=gv";
-        }
-        {
-          mode = "n";
-          key = "J";
-          action = "mzJ`z";
-        }
-        {
-          mode = "n";
-          key = "<C-d>";
-          action = "<C-d>zz";
-        }
-        {
-          mode = "n";
-          key = "<C-u>";
-          action = "<C-u>zz";
-        }
-        {
-          mode = "n";
-          key = "n";
-          action = "nzzzv";
-        }
-        {
-          mode = "n";
-          key = "N";
-          action = "Nzzzv";
-        }
-        {
-          mode = "v";
-          key = "<leader>y";
-          action = "\"+y";
-        }
-        {
-          mode = "n";
-          key = "k";
-          action = "v:count == 0 ? 'gk' : 'k'";
-          options = {
-            expr = true;
-            silent = true;
-          };
-        }
-        {
-          mode = "n";
-          key = "j";
-          action = "v:count == 0 ? 'gj' : 'j'";
-          options = {
-            expr = true;
-            silent = true;
-          };
-        }
-        {
-          mode = "n";
-          key = "<leader>q";
-          action.__raw = "vim.diagnostic.setloclist";
-        }
-        {
-          mode = "n";
-          key = "<leader>rn";
-          action.__raw = "vim.lsp.buf.rename";
-        }
-        {
-          mode = "n";
-          key = "K";
-          action.__raw = "vim.lsp.buf.hover";
-        }
-        {
-          mode = "n";
-          key = "<leader>u";
-          action.__raw = "vim.cmd.UndotreeToggle";
-        }
-        {
-          mode = "n";
-          key = "-";
-          action.__raw = "vim.cmd.Oil";
-        }
-        {
-          mode = "t";
-          key = "<esc><esc>";
-          action = "<C-\\><C-n>";
-        }
-        {
-          mode = "n";
-          key = "<leader>st";
-          action.__raw = ''
-               function()
-            if _G.mini_term_buf and vim.api.nvim_buf_is_valid(_G.mini_term_buf) then
-                for _, win in ipairs(vim.api.nvim_list_wins()) do
-            	local buf = vim.api.nvim_win_get_buf(win)
-            	if buf == _G.mini_term_buf then
-            	    vim.api.nvim_win_hide(win)
-            	    return
-            	end
-                end
-                vim.cmd.vsplit()
-                vim.api.nvim_win_set_buf(0, _G.mini_term_buf)
-                vim.cmd.wincmd("J")
-                vim.api.nvim_win_set_height(0, 15)
-                vim.cmd.startinsert()
-            else
-                vim.cmd.vnew()
-                vim.cmd.terminal()
-                vim.cmd.wincmd("J")
-                vim.api.nvim_win_set_height(0, 15)
-                vim.cmd.startinsert()
-                _G.mini_term_buf = vim.api.nvim_get_current_buf()
-            end
-               end
-          '';
-        }
-      ];
-      colorschemes.catppuccin = {
-        enable = true;
-        settings.flavor = "mocha";
-      };
-      plugins = {
-        cmp = {
-          enable = true;
-          autoEnableSources = true;
-          settings = {
-            mapping.__raw = ''
-              cmp.mapping.preset.insert({
-                  ['<Tab>'] = cmp.mapping.select_next_item(),
-                  ['<C-CR>'] = cmp.mapping.confirm({ select = true }),
-              })
-            '';
-            sources = [
-              {name = "nvim_lsp";}
-              {name = "path";}
-              {name = "buffer";}
-            ];
-          };
+        # stylix.targets.nvf.enable = false;
+        
+        programs.nvf = {
+            enable = true;
+            settings = {
+                vim = {
+                    viAlias = true;
+                    vimAlias = true;
+                    autocmds = [
+                        {
+                            event = [ "TextYankPost" ];
+                            pattern = [ "*" ];
+                            callback = lib.generators.mkLuaInline ''
+                                function()
+                                    vim.highlight.on_yank()
+                                end
+                            '';
+                        }
+                    ];
+                    globals = {
+                        mapleader = " ";
+                        maplocalleader = " ";
+                    };
+                    options = {
+                        breakindent = true;
+                        completeopt = "menuone,noselect";
+                        conceallevel = 2;
+                        expandtab = true;
+                        formatoptions = "ro/";
+                        hlsearch = false;
+                        ignorecase = true;
+                        incsearch = true;
+                        number = true;
+                        relativenumber = true;
+                        scrolloff = 8;
+                        shiftwidth = 4;
+                        signcolumn = "yes";
+                        smartcase = true;
+                        smartindent = true;
+                        softtabstop = 4;
+                        tabstop = 4;
+                        termguicolors = true;
+                        undofile = true;
+                        wrap = false;
+                        foldlevel = 99;
+                        foldlevelstart = 99;
+                    };
+                    terminal = {
+                        toggleterm = {
+                            enable = true;
+                            mappings.open = "<leader>st";
+                            setupOpts = {
+                                direction = "float";
+                                on_open = lib.generators.mkLuaInline ''
+                                    function()
+                                        vim.schedule(function()
+                                            vim.cmd("startinsert")
+                                        end)
+                                    end
+                                '';
+                            };
+                            lazygit.enable = true;
+                        };
+                    };
+                    autocomplete.blink-cmp.enable = true;
+                    git.gitsigns = {
+                        enable = true;
+                        setupOpts = {
+                            signs = {
+                                add.text = "+";
+                                change.text = "~";
+                                delete.text = "_";
+                                topdelete.text = "‾";
+                                changedelete.text = "~";
+                            };
+                            current_line_blame = true;
+                            current_line_blame_opts = {
+                                virt_text = false;
+                                delay = 0;
+                            };
+                        };
+                    };
+                    statusline.lualine = {
+                        enable = true;
+                        activeSection.c = [
+                            ''
+                                function()
+                                    local blame = vim.b.gitsigns_blame_line
+                                    if blame == nil or blame == "" then
+                                        return ""
+                                    end
+                                    return blame
+                                end
+                            ''
+                        ];
+                    };
+                    utility = {
+                        oil-nvim = {
+                            enable = true;
+                            setupOpts = {
+                                view_options.show_hidden = true;
+                                win_options.signcolumn = "yes:2";
+                            };
+                            gitStatus.enable = true;
+                        };
+                        undotree.enable = true;
+                    };
+                    ui.smartcolumn = {
+                        enable = true;
+                        setupOpts = {
+                            colorcolumn = "80";
+                            scope = "line";
+                        };
+                    };
+                    telescope = {
+                        enable = true;
+                        mappings = {
+                            findFiles = "<leader>ff";
+                            liveGrep = "<leader>fg";
+                        };
+                    };
+                    visuals.nvim-web-devicons.enable = true;
+                    binds.whichKey.enable = true;
+                    lsp = {
+                        enable = true;
+                        lspconfig.enable = true;
+                    };
+                    treesitter = {
+                        enable = true;
+                        fold = true;
+                        context.enable = true;
+                    };
+                    languages = {
+                        enableTreesitter = true;
+                        enableFormat = true;
+                        enableExtraDiagnostics = true;
+                        nix.enable = true;
+                    };
+                    keymaps = [
+                        {
+                            mode = [ "n" "v" ];
+                            key = "<Space>";
+                            action = "<Nop>";
+                            silent = true;
+                        }
+                        {
+                            mode = "v";
+                            key = "J";
+                            action = ":m '>+1<CR>gv=gv";
+                        }
+                        {
+                            mode = "v";
+                            key = "K";
+                            action = ":m '<-2<CR>gv=gv";
+                        }
+                        {
+                            mode = "n";
+                            key = "J";
+                            action = "mzJ`z";
+                        }
+                        {
+                            mode = "n";
+                            key = "<C-d>";
+                            action = "<C-d>zz";
+                        }
+                        {
+                            mode = "n";
+                            key = "<C-u>";
+                            action = "<C-u>zz";
+                        }
+                        {
+                            mode = "n";
+                            key = "n";
+                            action = "nzzzv";
+                        }
+                        {
+                            mode = "n";
+                            key = "N";
+                            action = "Nzzzv";
+                        }
+                        {
+                            mode = "v";
+                            key = "<leader>y";
+                            action = "\"+y";
+                        }
+                        {
+                            mode = "n";
+                            key = "k";
+                            action = "v:count == 0 ? 'gk' : 'k'";
+                            expr = true;
+                            silent = true;
+                        }
+                        {
+                            mode = "n";
+                            key = "j";
+                            action = "v:count == 0 ? 'gj' : 'j'";
+                            expr = true;
+                            silent = true;
+                        }
+                        {
+                            mode = "n";
+                            key = "<leader>q";
+                            action = "vim.diagnostic.setloclist";
+                            lua = true;
+                        }
+                        {
+                            mode = "n";
+                            key = "<leader>rn";
+                            action = "vim.lsp.buf.rename";
+                            lua = true;
+                        }
+                        {
+                            mode = "n";
+                            key = "K";
+                            action = "vim.lsp.buf.hover";
+                            lua = true;
+                        }
+                        {
+                            mode = "n";
+                            key = "<leader>u";
+                            action = "vim.cmd.UndotreeToggle";
+                            lua = true;
+                        }
+                        {
+                            mode = "n";
+                            key = "-";
+                            action = "vim.cmd.Oil";
+                            lua = true;
+                        }
+                        {
+                            mode = "t";
+                            key = "<esc><esc>";
+                            action = "<C-\\><C-n>";
+                        }
+                    ];
+                };
+            };
         };
-        gitblame = {
-          enable = true;
-          settings.display_virtual_text = false;
-        };
-        gitsigns = {
-          enable = true;
-          settings.signs = {
-            add.text = "+";
-            change.text = "~";
-            delete.text = "_";
-            topdelete.text = "‾";
-            changedelete.text = "~";
-          };
-        };
-        lspconfig.enable = true;
-        lualine = {
-          enable = true;
-          settings.sections.lualine_c = [
-            {
-              __unkeyed-1.__raw = "require('gitblame').get_current_blame_text";
-              cond.__raw = "require('gitblame').is_blame_text_available";
-            }
-          ];
-        };
-        oil = {
-          enable = true;
-          settings = {
-            view_options.show_hidden = true;
-            win_options.signcolumn = "yes:2";
-          };
-        };
-        oil-git-status.enable = true;
-        smartcolumn = {
-          enable = true;
-          settings = {
-            colorcolumn = "80";
-            scope = "line";
-          };
-        };
-        telescope = {
-          enable = true;
-          keymaps = {
-            "<leader>ff" = "find_files";
-            "<leader>fg" = "live_grep";
-          };
-        };
-        treesitter = {
-          enable = true;
-          settings.highlight.enable = true;
-          grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
-            nix
-            lua
-            go
-          ];
-        };
-        treesitter-context.enable = true;
-        undotree.enable = true;
-        web-devicons.enable = true;
-        which-key.enable = true;
-      };
-      lsp.servers = {
-        nil_ls.enable = true;
-        lua_ls.enable = true;
-        gopls.enable = true;
-      };
     };
-  };
 }
