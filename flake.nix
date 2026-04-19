b1cb17e77061c9c3e8d8e26191109a64a300db73
jan <jan.reyes.contact@gmail.com>
2026-04-18 19:31:36 -0600

    added nvf to flake inputs

--- flake.nix
+++ flake.nix
@@ -26,6 +26,10 @@
             url = "github:nix-community/stylix";
             inputs.nixpkgs.follows = "nixpkgs";
         };
+        nvf = {
+            url = "github:NotAShelf/nvf";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
 	};
 
 	outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
