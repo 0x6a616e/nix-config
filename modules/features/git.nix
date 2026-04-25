79e2e6fe03f02a1c23f77cc85881a27cae9ef05a
jan <jan.reyes.contact@gmail.com>
2026-04-24 23:10:30 -0600

    set default git branch name

--- modules/features/git.nix
+++ modules/features/git.nix
@@ -3,6 +3,9 @@ _: {
         programs.git = {
             enable = true;
             settings = {
+                init = {
+                    defaultBranch = "main";
+                };
                 user = {
                     name = "jan";
                     email = "jan.reyes.contact@gmail.com";
