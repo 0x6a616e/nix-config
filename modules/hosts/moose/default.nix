abee1d431323e36d22fb27f818af3c04c8de63bf
jan <jan.reyes.contact@gmail.com>
2026-04-26 22:35:26 -0600

    added moose age key and activated tailscale

--- modules/hosts/moose/default.nix
+++ modules/hosts/moose/default.nix
@@ -10,7 +10,7 @@
             self.nixosModules.impermanence
             # self.nixosModules.jan
             self.nixosModules.nh
-            # self.nixosModules.tailscale
+            self.nixosModules.tailscale
             # self.nixosModules.zsh
         ];
 
