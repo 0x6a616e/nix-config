db360b889b3ebf9768d2a7ced4f4fbfa1481441d
jan <jan.reyes.contact@gmail.com>
2026-04-26 23:50:44 -0600

    updated kitty

--- modules/features/kitty.nix
+++ modules/features/kitty.nix
@@ -1,11 +1,16 @@
 _: {
-    flake.homeModules.kitty = _: {
-        programs.kitty = {
-            enable = true;
-            shellIntegration.enableZshIntegration = true;
-            settings = {
-                window_padding_width = 5;
-                hide_window_decorations = "yes";
+    flake.homeModules.kitty = { lib, config, ... }: {
+        options = {
+            kitty.enable = lib.mkEnableOption "enable kitty";
+        };
+        config = lib.mkIf config.kitty.enable {
+            programs.kitty = {
+                enable = true;
+                shellIntegration.enableZshIntegration = true;
+                settings = {
+                    window_padding_width = 5;
+                    hide_window_decorations = "yes";
+                };
             };
         };
     };
