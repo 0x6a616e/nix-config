f1569301ca517728068a2ef6596dc6da888257e3
jan <jan.reyes.contact@gmail.com>
2026-04-26 23:51:00 -0600

    updated ripgrep

--- modules/features/ripgrep.nix
+++ modules/features/ripgrep.nix
@@ -1,5 +1,10 @@
 _: {
-    flake.homeModules.ripgrep = _: {
-        programs.ripgrep.enable = true;
+    flake.homeModules.ripgrep = { lib, config, ... }: {
+        options = {
+            ripgrep.enable = lib.mkEnableOption "enable ripgrep";
+        };
+        config = lib.mkIf config.ripgrep.enable {
+            programs.ripgrep.enable = true;
+        };
     };
 }
