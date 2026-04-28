866ebded7a1ae81331dc1952e968867b3ac32477
jan <jan.reyes.contact@gmail.com>
2026-04-28 00:02:26 -0600

    moved one option

--- modules/features/kitty.nix
+++ modules/features/kitty.nix
@@ -6,11 +6,11 @@ _: {
         config = lib.mkIf config.kitty.enable {
             programs.kitty = {
                 enable = true;
-                shellIntegration.enableZshIntegration = true;
                 settings = {
                     window_padding_width = 5;
                     hide_window_decorations = "yes";
                 };
+                shellIntegration.enableZshIntegration = true;
             };
         };
     };
