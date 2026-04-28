6ced80e93a98d6f0e49bcd0b1bc9d6332fd8a4b7
jan <jan.reyes.contact@gmail.com>
2026-04-27 23:09:14 -0600

    added some btop config

--- modules/features/btop.nix
+++ modules/features/btop.nix
@@ -1,12 +1,17 @@
 _: {
-    flake.homeModules.btop = { lib, config, ... } : {
+    flake.homeModules.btop = { lib, config, pkgs, ... } : {
         options = {
             btop.enable = lib.mkEnableOption "enable btop";
         };
         config = lib.mkIf config.btop.enable {
             programs.btop = {
                 enable = true;
+                package = pkgs.btop.override {
+                    rocmSupport = true;
+                };
                 settings = {
+                    cpu_single_graph = true;
+                    shown_boxes = "cpu mem net proc gpu0";
                     vim_keys = true;
                 };
             };
