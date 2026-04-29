a0c7a1a336df86e0fdfa420d3d5c4fd794b44a9a
jan <jan.reyes.contact@gmail.com>
2026-04-28 23:10:27 -0600

    removed failsafe from config

--- modules/hosts/default.nix
+++ modules/hosts/default.nix
@@ -1,8 +1,4 @@
 { self, inputs, ... }: {
-    flake.nixosConfigurations.failsafe = inputs.nixpkgs.lib.nixosSystem {
-        modules = [ self.nixosModules.failsafeConfiguration ];
-    };
-
     flake.nixosConfigurations.moose = inputs.nixpkgs.lib.nixosSystem {
         modules = [ self.nixosModules.mooseConfiguration ];
     };
