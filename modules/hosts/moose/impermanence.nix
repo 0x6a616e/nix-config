8dc384519a69dbad86d844049eff0ae8cfc42ffd
jan <jan.reyes.contact@gmail.com>
2026-04-30 14:37:31 -0600

    sorted files and directories

--- modules/hosts/moose/impermanence.nix
+++ modules/hosts/moose/impermanence.nix
@@ -16,34 +16,34 @@
                 enable = true;
                 hideMounts = true;
                 directories = [
-                    "/var/log"
+                    { directory = "/var/lib/colord"; user = "colord"; group = "colord"; mode = "u=rwx,g=rx,o="; }
+                    "/etc/NetworkManager/system-connections"
+                    "/etc/sops/"
                     "/var/lib/bluetooth"
                     "/var/lib/nixos"
-                    "/var/lib/systemd/coredump"
-                    "/etc/NetworkManager/system-connections"
-                    { directory = "/var/lib/colord"; user = "colord"; group = "colord"; mode = "u=rwx,g=rx,o="; }
-                    "/etc/sops/"
-                    "/var/lib/tailscale"
                     "/var/lib/power-profiles-daemon"
+                    "/var/lib/systemd/coredump"
+                    "/var/lib/tailscale"
+                    "/var/log"
                 ];
                 files = [
                     "/etc/machine-id"
                 ];
                 users.jan = {
                     directories = [
-                        { directory = ".ssh"; mode = "0700"; }
                         { directory = ".config/sops"; mode = "0700"; }
-                        "nix-config"
-                        ".password-store"
                         { directory = ".gnupg"; mode = "0700"; }
-                        ".local/state/wireplumber"
-                        ".local/share/zoxide"
-                        ".local/share/zsh"
-                        "Downloads"
-                        ".mozilla/firefox/main"
+                        { directory = ".ssh"; mode = "0700"; }
                         ".local/state/nvf/undo"
                         ".local/share/direnv"
+                        ".local/share/zoxide"
+                        ".local/share/zsh"
+                        ".local/state/wireplumber"
+                        ".mozilla/firefox/main"
+                        ".password-store"
                         "dev"
+                        "Downloads"
+                        "nix-config"
                     ];
                     files = [
                         ".config/lazygit/state.yml"
