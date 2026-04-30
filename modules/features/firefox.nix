f1b192673c46eda3cb7215c5d7a4cb7da77d441d
jan <jan.reyes.contact@gmail.com>
2026-04-30 12:10:05 -0600

    removed search engines and added a bookmark

--- modules/features/firefox.nix
+++ modules/features/firefox.nix
@@ -8,48 +8,13 @@ _: {
                 enable = true;
                 configPath = ".mozilla/firefox";
                 profiles.main = {
-                    extensions.force = true;
-                    search = {
+                    bookmarks = {
                         force = true;
-                        engines = {
-                            nix-packages = {
-                                name = "Nix Packages";
-                                urls = [{
-                                    template = "https://search.nixos.org/packages";
-                                    params = [
-                                        { name = "channel"; value = "unstable"; }
-                                        { name = "query"; value = "{searchTerms}"; }
-                                    ];
-                                }];
-                                icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-                                definedAliases = [ "@np" ];
-                            };
-                            nix-options = {
-                                name = "Nix Options";
-                                urls = [{
-                                    template = "https://search.nixos.org/options";
-                                    params = [
-                                        { name = "channel"; value = "unstable"; }
-                                        { name = "query"; value = "{searchTerms}"; }
-                                    ];
-                                }];
-                                icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-                                definedAliases = [ "@no" ];
-                            };
-                            home-options = {
-                                name = "Home Manager Options";
-                                urls = [{
-                                    template = "https://home-manager-options.extranix.com/";
-                                    params = [
-                                        { name = "release"; value = "master"; }
-                                        { name = "query"; value = "{searchTerms}"; }
-                                    ];
-                                }];
-                                icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-                                definedAliases = [ "@ho" ];
-                            };
-                        };
+                        settings = [
+                            { name = "NixOs Search"; url = "https://search.nixos.org/packages?channel=unstable"; keyword = "ns"; }
+                        ];
                     };
+                    extensions.force = true;
                 };
             };
         };
