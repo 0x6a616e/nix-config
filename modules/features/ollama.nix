c5de595a21f5f409b12e1c966d65b2ac60ca1fe8
jan <jan.reyes.contact@gmail.com>
2026-04-30 14:46:27 -0600

    removed preloaded models

--- modules/features/ollama.nix
+++ modules/features/ollama.nix
@@ -1,12 +1,5 @@
 _: {
     flake.nixosModules.ollama = _: {
-        services.ollama = {
-            enable = true;
-            loadModels = [
-                "qwen2.5:14b"
-                "qwen2.5:32"
-            ];
-            syncModels = true;
-        };
+        services.ollama.enable = true;
     };
 }
