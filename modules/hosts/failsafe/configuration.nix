72d522b579c8e94c98b73a7eb313fbe321db999b
jan <jan.reyes.contact@gmail.com>
2026-05-02 00:09:21 -0600

    added my user to trusted users for remote build

--- modules/hosts/failsafe/configuration.nix
+++ modules/hosts/failsafe/configuration.nix
@@ -68,6 +68,7 @@
         nix.settings = {
             auto-optimise-store = true;
             experimental-features = [ "nix-command" "flakes" ];
+            trusted-users = [ "jan" ];
         };
 
         nixpkgs = {
