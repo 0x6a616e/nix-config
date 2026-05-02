38e2b1c897f2f91e5a85b2d7d4b23341072cf05f
jan <jan.reyes.contact@gmail.com>
2026-05-01 22:38:58 -0600

    updated disk config of failsafe

--- modules/hosts/failsafe/disko.nix
+++ modules/hosts/failsafe/disko.nix
@@ -5,34 +5,52 @@
         ];
 
         disko.devices = {
-            disk.main = {
-                type = "disk";
-                device = "/dev/disk/by-id/nvme-SAMSUNG_MZVL81T0HFLB-00BLL_S7XKNF0Y607708";
-                content = {
-                    type = "gpt";
-                    partitions = {
-                        ESP = {
-                            size = "4G";
-                            type = "EF00";
-                            content = {
-                                type = "filesystem";
-                                format = "vfat";
-                                mountpoint = "/boot";
-                                mountOptions = [ "umask=0077" ];
+            disk = {
+                disk1 = {
+                    type = "disk";
+                    device = "/dev/disk/by-id/nvme-SAMSUNG_MZVL81T0HFLB-00BLL_S7XKNF0Y607708";
+                    content = {
+                        type = "gpt";
+                        partitions = {
+                            ESP = {
+                                size = "4G";
+                                type = "EF00";
+                                content = {
+                                    type = "filesystem";
+                                    format = "vfat";
+                                    mountpoint = "/boot";
+                                    mountOptions = [ "umask=0077" ];
+                                };
+                            };
+                            zfs = {
+                                end = "-1G";
+                                content = {
+                                    type = "zfs";
+                                    pool = "rpool";
+                                };
+                            };
+                            swap = {
+                                size = "16G";
+                                content = {
+                                    type = "swap";
+                                    discardPolicy = "both";
+                                };
                             };
                         };
-                        zfs = {
-                            end = "-1G";
-                            content = {
-                                type = "zfs";
-                                pool = "rpool";
-                            };
-                        };
-                        swap = {
-                            size = "8G";
-                            content = {
-                                type = "swap";
-                                discardPolicy = "both";
+                    };
+                };
+                disk2 = {
+                    type = "disk";
+                    device = "/dev/disk/by-id/nvme-KINGSTON_SNV3S1000G_50026B7687129744";
+                    content = {
+                        type = "gpt";
+                        partitions = {
+                            zfs = {
+                                end = "-1G";
+                                content = {
+                                    type = "zfs";
+                                    pool = "rpool";
+                                };
                             };
                         };
                     };
@@ -40,6 +58,7 @@
             };
             zpool = {
                 rpool = {
+                    mode = "mirror";
                     options.cachefile = "none";
                     rootFsOptions.compression = "zstd";
                     type = "zpool";
