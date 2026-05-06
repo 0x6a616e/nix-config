c015f8441cb08108130110a89ac8583afd453fe0
jan <jan.reyes.contact@gmail.com>
2026-05-05 22:26:35 -0600

    removed gnome config

--- modules/features/gnome.nix
+++ modules/features/gnome.nix
@@ -17,70 +17,17 @@ _: {
             gnome.enable = lib.mkEnableOption "enable gnome";
         };
         config = lib.mkIf config.gnome.enable {
-            home = {
-                file.".config/monitors.xml".source = ../../assets/monitors.xml;
-                packages = with pkgs; [
-                    collision
-                    gnome-characters
-                    loupe
-                    nautilus
-                    showtime
-                    wl-clipboard
-                    gnomeExtensions.caffeine
-                ];
-            };
+            home.packages = with pkgs; [
+                collision
+                gnome-characters
+                loupe
+                nautilus
+                showtime
+                wl-clipboard
+                gnomeExtensions.caffeine
+            ];
 
             gtk.gtk4.theme = null;
-
-            dconf = {
-                enable = true;
-                settings = {
-                    "org/gnome/desktop/app-folders" = {
-                        folder-children = [ ];
-                    };
-                    "org/gnome/desktop/input-sources" = {
-                        sources = [ (lib.hm.gvariant.mkTuple [ "xkb" "us" ]) ];
-                        xkb-options = [ "compose:ralt" ];
-                    };
-                    "org/gnome/desktop/interface" = {
-                        accent-color = "yellow";
-                        clock-format = "24h";
-                    };
-                    "org/gnome/desktop/notifications" = {
-                        application-children = [ "org-gnome-nautilus" ];
-                    };
-                    "org/gnome/desktop/notifications/application/org-gnome-nautilus" = {
-                        application-id = "org.gnome.Nautilus.desktop";
-                    };
-                    "org/gnome/desktop/peripherals/mouse" = {
-                        accel-profile = "flat";
-                        speed = 0.0;
-                    };
-                    "org/gnome/desktop/screen-time-limits" = {
-                        daily-limit-enabled = false;
-                        history-enabled = false;
-                    };
-                    "org/gnome/desktop/search-providers" = {
-                        disable-external = true;
-                    };
-                    "org/gnome/desktop/session" = {
-                        idle-delay = lib.hm.gvariant.mkUint32 300;
-                    };
-                    "org/gnome/desktop/wm/keybindings" = {
-                        cycle-windows = [];
-                        cycle-windows-backward = [];
-                        switch-group = [ "<Alt>grave" ];
-                        switch-group-backward = [ "<Shift><Alt>grave" ];
-                    };
-                    "org/gnome/settings-daemon/plugins/power" = {
-                        sleep-inactive-ac-timeout = 900;
-                    };
-                    "org/gnome/shell" = {
-                        enabled-extensions = [ "caffeine@patapon.info" ];
-                        favorite-apps = [];
-                    };
-                };
-            };
         };
     };
 }
