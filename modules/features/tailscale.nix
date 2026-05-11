0c79d5f66092d34bcb2cbd145995a91774175961
jan <jan.reyes.contact@gmail.com>
2026-05-10 23:08:54 -0600

    added tailscale health service

--- modules/features/tailscale.nix
+++ modules/features/tailscale.nix
@@ -1,4 +1,30 @@
 { self, ... }: {
+    flake.nixosModules.tailscaleHealthService = { lib, pkgs, ... }: {
+        systemd = {
+            timers."tailscale-health" = {
+                wanted-by = [ "timers.target" ];
+                timerConfig = {
+                    OnCalendar = "*:00:00";
+                    Unit = "tailscale-health";
+                };
+            };
+            services."tailscale-health" = {
+                script = /* bash */ ''
+                    set -eu
+                    result=\$(${lib.getExe pkgs.tailscale} status --json | ${lib.getExe pkgs.jq} '.Health')
+                    if [[ \$result != "[]" ]]; then
+                        reboot;
+                    fi
+                '';
+                serviceConfig = {
+                    Type = "oneshot";
+                    User = "root";
+                    RemainAfterExit = true;
+                };
+            };
+        };
+    };
+
     flake.nixosModules.tailscale = { config, ... }: {
         imports = [
             self.nixosModules.sops
