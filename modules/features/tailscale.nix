81d1711901b6eb82dff93cbffe2731893d976e41
jan <jan.reyes.contact@gmail.com>
2026-05-10 23:13:52 -0600

    typo

--- modules/features/tailscale.nix
+++ modules/features/tailscale.nix
@@ -11,7 +11,7 @@
             services."tailscale-health" = {
                 script = /* bash */ ''
                     set -eu
-                    result=\$(${lib.getExe pkgs.tailscale} status --json | ${lib.getExe pkgs.jq} '.Health')
+                    result=$(${lib.getExe pkgs.tailscale} status --json | ${lib.getExe pkgs.jq} '.Health')
                     if [[ \$result != "[]" ]]; then
                         reboot;
                     fi
