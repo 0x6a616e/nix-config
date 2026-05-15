154462b8e4e958747454bf6ea994f22a8dc2e1d9
jan <jan.reyes.contact@gmail.com>
2026-05-15 17:55:08 -0600

    removed for now

--- modules/hosts/chappie/configuration.nix
+++ modules/hosts/chappie/configuration.nix
@@ -69,7 +69,6 @@
         nix.settings = {
             auto-optimise-store = true;
             experimental-features = [ "nix-command" "flakes" ];
-            substituters = lib.mkForce [ ];
         };
 
         nixpkgs = {
