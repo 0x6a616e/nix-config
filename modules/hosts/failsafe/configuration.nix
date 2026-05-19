3626f98b1a184b025d996d7e2c49ebc0b655a0ec
jan <jan.reyes.contact@gmail.com>
2026-05-18 22:18:07 -0600

    removed unused line

--- modules/hosts/failsafe/configuration.nix
+++ modules/hosts/failsafe/configuration.nix
@@ -51,7 +51,6 @@
         nix.settings = {
             auto-optimise-store = true;
             experimental-features = [ "nix-command" "flakes" ];
-            trusted-users = [ "jan" ];
         };
 
         nixpkgs = {
