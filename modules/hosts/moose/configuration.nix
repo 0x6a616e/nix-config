5ff3cd0dc116966a91a61ccb4fb4fb5d8fa2cc84
jan <jan.reyes.contact@gmail.com>
2026-05-29 18:27:26 -0600

    nvm

--- modules/hosts/moose/configuration.nix
+++ modules/hosts/moose/configuration.nix
@@ -1,5 +1,5 @@
 { self, inputs, ... }: {
-    flake.nixosModules.mooseConfiguration = { config, lib, modulesPath, pkgs, ... }: {
+    flake.nixosModules.mooseConfiguration = { config, lib, modulesPath, ... }: {
         imports = [
             (modulesPath + "/installer/scan/not-detected.nix")
             inputs.home-manager.nixosModules.home-manager
@@ -17,7 +17,6 @@
                 kernelModules = [ ];
             };
             kernelModules = [ "kvm-amd" ];
-            kernelPackages = pkgs.linuxPackages_latest;
             loader = {
                 efi.canTouchEfiVariables = true;
                 timeout = 30;
