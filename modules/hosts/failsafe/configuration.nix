0f5026ca4ae863bdac73a8dcdb9db6dedca1761a
jan <jan.reyes.contact@gmail.com>
2026-08-13 17:33:56 -0600

    updated some config for failsafe

--- modules/hosts/failsafe/configuration.nix
+++ modules/hosts/failsafe/configuration.nix
@@ -9,10 +9,11 @@
         boot = {
             extraModulePackages = [ ];
             initrd = {
-                availableKernelModules = [ "nvme" "xhci_pci" "usb_storage" "sd_mod" ];
+                availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
                 kernelModules = [ ];
             };
             kernelModules = [ "kvm-amd" ];
+            kernelPackages = pkgs.linuxPackages_latest;
             loader = {
                 efi.canTouchEfiVariables = true;
                 timeout = 30;
